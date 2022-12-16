bdbb8e51f63fcb848512a6a15ebd56bfbd6952c6
tonchez2700 <eduardo.tonchez@copixil.com>
2022-12-16 17:54:50 -0600

    cambio de tipo de pago de tarjeta y rediccionamie

--- src/screens/CardPaymentScreen.js
+++ src/screens/CardPaymentScreen.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react'
 import {
-    StyleSheet, View, ScrollView, TouchableOpacity,
-    Text, Dimensions, TextInput, Image
+    StyleSheet, View
 } from 'react-native';
 import { Icon, Button, Input } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native';
 import { Context as AccountDataContext } from '../context/AccountDataContext';
 import WebView from 'react-native-webview';
-import Images from '@assets/images';
-import MaskInput, { Masks } from 'react-native-mask-input';
-import tw from 'tailwind-react-native-classnames'
-import moment from 'moment';
 
-const { width } = Dimensions.get('window');
 const CardPaymentScreen = (props) => {
+
     const navigation = useNavigation();
     const { state
     } = useContext(AccountDataContext);
@@ -22,10 +17,23 @@ const CardPaymentScreen = (props) => {
 
         <View style={{ flex: 1, backgroundColor: '#ECECEC', padding: 17, marginTop: 4, }}>
             <WebView
+                style={{ marginTop: 20 }}
+                startInLoadingState={true}
+                useWebKit={false}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                scalesPageToFit={true}
+                automaticallyAdjustContentInsets={true}
                 source={{
                     uri: props.route.params.payment_method.url
                 }}
-                style={{ marginTop: 20 }}
+                onNavigationStateChange={(webViewState) => {
+                    console.log(webViewState)
+                    if (webViewState.url != props.route.params.payment_method.url) {
+                        //navigate or close webview
+                        navigation.navigate('Inicio')
+                    }
+                }}
             />
         </View >
     )
@@ -35,34 +43,4 @@ export default CardPaymentScreen
 
 const styles = StyleSheet.create({
 
-    titlleText: {
-        textAlign: 'center',
-        fontSize: 15,
-        fontWeight: 'bold',
-        marginBottom: 15
-    },
-    titlle2Text: {
-        textAlign: 'center',
-        fontSize: 11,
-        marginBottom: 15
-    },
-    InputText: {
-        textAlign: 'left',
-        fontWeight: 'bold',
-        fontSize: 12,
-    },
-    DataText: {
-        textAlign: 'left',
-        fontSize: 12,
-    },
-    InputPayments: {
-        borderWidth: 1,
-        height: 35,
-        paddingVertical: 9,
-        paddingLeft: 9,
-        borderRadius: 4,
-        borderColor: '#707070',
-        marginTop: 4,
-        marginBottom: 6,
-    },
 })
